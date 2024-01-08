5ce76c2b884cd2e88dfd3f866a8a10f65a287b23
Rey <rey.robles72@gmail.com>
2024-01-08 06:09:26 -0600

    Add files via upload

---
+++ mnistpixgenerator.py
@@ -0,0 +1,20 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+width = 28
+height = 28
+
+
+def random_pixel_generator(width, height):
+    image = np.zeros((height, width))
+    for i in range(height):
+        for j in range(width):        image[i, j] = int(torch.rand(1)*2)
+    print(image)
+    return image
+
+
+t1 = torch.as_tensor(random_pixel_generator(width, height))
+print(t1.size())
+
